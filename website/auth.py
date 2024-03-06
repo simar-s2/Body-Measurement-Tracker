3ca92ccfacb6abe08dfc41b3d725e65f12705488
simar-s2 <simarjot2468@gmail.com>
2024-03-06 16:54:17 +0300

    Added working bootstrap for navbar in layout.html

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 auth = Blueprint('auth', __name__)
 
@@ -8,7 +8,7 @@ def login():
 
 @auth.route('/logout')
 def logout():
-    return 'logout page'
+    return redirect('/login')
 
 @auth.route('/sign-up')
 def sign_up():
