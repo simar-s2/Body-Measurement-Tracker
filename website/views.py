720b1597fe3889a6dacb7abf58bb8086e0a3b5c4
simar-s2 <simarjot2468@gmail.com>
2024-03-12 02:13:02 +0300

    Add measurements page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
@@ -7,4 +7,12 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/measurement', methods=['GET', 'POST'])
+@login_required
+def measurement():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template("measurement.html", user=current_user)
     
